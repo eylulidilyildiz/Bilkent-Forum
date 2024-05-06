bb0691cdf8aecace46ca963fa2cab5b7e51504e7
Berkay Ataman <145963468+BerkayAtmn@users.noreply.github.com>
2024-05-06 13:09:18 +0300

    Delete FXtest/fxtest directory

--- FXtest/fxtest/src/main/java/com/example/QuestionAndAnswerPost.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class QuestionAndAnswerPost extends Post 
-{
-    public QuestionAndAnswerPost(String description)    
-    {
-        this.description = description;
-    }
-}
