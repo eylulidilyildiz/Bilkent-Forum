2a47de9ed0687e3ae6f98f0ab4badf21e16ce1f1
Berkay Ataman <berkayautmn@gmail.com>
2024-05-07 19:12:05 +0300

    added new requerment

--- demofx/demofx/src/main/java/module-info.java
+++ demofx/demofx/src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module com.example {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires jakarta.persistence;
     requires java.sql;
     requires org.hibernate.orm.core;
     
-    opens com.example to javafx.fxml, org.hibernate.orm.core;
+    opens com.example to javafx.fxml, javafx.graphics, org.hibernate.orm.core;
     exports com.example;
 }
