e6957b272a117b72edb03d7486240911f448236d
Berkay Ataman <berkayautmn@gmail.com>
2024-05-10 14:23:49 +0300

    Comment implementation

---
+++ demofx/demofx/src/main/java/com/example/CommentManager.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class CommentManager 
+{
+    public void createPost(int id, String content, int ownerID, int commentedPostID) 
+    {
+        Comment newComment = new Comment();
+        newComment.setId(id);
+        newComment.setContent(content);
+        newComment.setOwnerID(ownerID);
+        newComment.setCommentedPostID(commentedPostID);
+
+        Session session = DatabaseConnection.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            session.persist(newComment);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void deleteComment(int commentID) 
+    {
+        Session session = DatabaseConnection.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            Comment commentToDelete = session.get(Comment.class, commentID); 
+            if (commentToDelete != null) {
+                session.remove(commentID); 
+                tx.commit();
+            } 
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+}
