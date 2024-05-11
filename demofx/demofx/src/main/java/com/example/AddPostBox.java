f08d1da2174d1bbebd9ffa128a9989e55602f5c2
Eylül İdil Yıldız <eylulidilyildiz@gmail.com>
2024-05-11 18:33:09 +0300

    addPost

--- demofx/demofx/src/main/java/com/example/AddPostBox.java
+++ demofx/demofx/src/main/java/com/example/AddPostBox.java
@@ -27,49 +27,32 @@ public class AddPostBox extends VBox {
 
     // Instance Variables
     private User mainUser;
-    private HBox searchAddPostBox;
-    private VBox nameDepartmentSemesterBox;
-    private VBox mailPasswordBox;
-    private Button saveButton;
+    private HBox searchBox;
+    private VBox postInfoBox;
+    private VBox DescriptionAndPostTypeBox;
+    private HBox BookDetailsBox;
+    private VBox InfoFieldsBox;
+    private VBox CourseUsageAndPriceBox;
+    private Button createPostButton;
 
     public AddPostBox(User user)
     {
         super();
         mainUser = user;
-        this.createSearchAndAddPost();
+        this.createSearchBox();
 
         HBox addPostBox = new HBox();
         addPostBox.setSpacing (500);
-        //addPostBox.getChildren().addAll (this.nameDepartmentSemesterBox, this.mailPasswordBox);
+        addPostBox.getChildren().addAll (this.nameDepartmentSemesterBox, this.mailPasswordBox);
         this.getChildren().add (addPostBox);
     }
 
-     @SuppressWarnings("unchecked")
-    private void createNameDepartmentSemesterBox ()
+
+
+
+     private void createSearchBox ()
     {
-        nameDepartmentSemesterBox = new VBox();
-        nameDepartmentSemesterBox.setAlignment (Pos.CENTER_LEFT);
-
-        // name label and text field
-       
-        //nameDepartmentSemesterBox.getChildren().addAll (nameLabel, nameTextField, departmentLabel, departmentTextField, semesterLabel, semesterBox);
-
-
-    }
-
-    private void createMailPasswordBox ()
-    {
-        this.mailPasswordBox = new VBox();
-        this.mailPasswordBox.setAlignment (Pos.CENTER_LEFT);
-        
-        //this.mailPasswordBox.getChildren().addAll (mailLabel, mailTextField, passwordLabel, passwordTextField, passAgainLabel, passAgainTextField);
-    
-    }
-
-
-     private void createSearchAndAddPost ()
-    {
-        this.searchAddPostBox = new HBox();
+        this.searchBox = new HBox();
 
         // SEARCH BAR
         GridPane searchPane = new GridPane();
@@ -82,42 +65,14 @@ public class AddPostBox extends VBox {
         searchBar.setPrefHeight (40);
         searchBar.setPrefWidth (400);
         searchPane.add (searchBar, 2, 1);
-        
-        ImageView searchIcon = new ImageView (getClass().getResource("images/browseIcon.png").toString());
-        searchIcon.setFitHeight (ICON_HEIGHT);
-        searchIcon.setFitWidth (ICON_WIDTH);
-        searchPane.add (searchIcon, 0, 1); // image
 
         Label searchLabel = new Label ("Search:");
         searchLabel.setFont(Font.font("Tahoma", FontWeight.NORMAL, FontPosture.REGULAR, 22));
         searchPane.add (searchLabel, 1, 1);
 
-
-        // ADD POST BUTTON
-        GridPane postButtonPane = new GridPane();
-        postButtonPane.setAlignment (Pos.TOP_RIGHT);
-        postButtonPane.setHgap (10);
-        postButtonPane.setVgap (10);
-        postButtonPane.setPadding(new Insets(70));
-
-        ToggleButton addPostButton = new ToggleButton ();
-        addPostButton.setBackground (new Background(new BackgroundFill(null, CornerRadii.EMPTY, Insets.EMPTY)));
-        addPostButton.setPrefHeight (60);
-        addPostButton.setPrefWidth (60);
-
-        ImageView plusIcon = new ImageView (getClass().getResource("images/plusIcon.png").toString());
-        plusIcon.setFitHeight (60);
-        plusIcon.setFitWidth (60);
-
-        addPostButton.setGraphic (plusIcon);
-
-        postButtonPane.add (addPostButton, 0, 1);
-
-        // combining the searchPane and the addPostButton 
-        searchAddPostBox.setSpacing (550);
-        searchAddPostBox.getChildren().addAll (searchPane, postButtonPane);
-
-        this.getChildren().add (searchAddPostBox);
+        searchBox.setSpacing (550);
+        searchBox.getChildren().add(searchPane);
+        this.getChildren().add (searchBox);
         
     }
 
